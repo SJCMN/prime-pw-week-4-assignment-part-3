eb5c3eda7664c7079477ddfa427500c1360e9f1a
SJCMN <sam.clavette@gmail.com>
2021-09-12 16:26:07 -0500

    changed basket from const to let, to easily clear contents

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,7 +1,7 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-const basket = []
+let basket = []
 
 function addItem ( item ) {
   basket.push(item);
@@ -22,15 +22,30 @@ function listItems ( yourBasket ){
   return yourBasket;
 }
 
+
 console.log('Your basket contains: ', listItems(basket));
 
-function empty (basketArr) {
-  basketArr = [];
-  return basketArr;
+function empty () {
+  basket = [];
+  return basket;
 }
 
-console.log('Your cart is now empty: ', empty(basket));
-
 console.log(`Basket is ${basket}`);
+
+console.log('Your cart is now empty: ', empty());
+
 console.log(`Adding apples to basket`, addItem('apples'));
+
 console.log(`Basket is now ${basket}`);
+
+const maxItems = 5;
+
+function full() {
+  if (basket.length >= maxItems){
+    return true;
+  } else if (basket.length < maxItems) {
+    return false;
+  }
+}
+console.log(basket, basket.length);
+console.log('Your basket is full' ,full());
