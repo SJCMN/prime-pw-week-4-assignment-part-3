cedbf20fdf0f50ac848222b6fe8a6ade8b8ddeac
SJCMN <sam.clavette@gmail.com>
2021-09-12 16:52:31 -0500

    Update addItem with full basket logic

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,20 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = []
+const maxItems = 5;
 
 function addItem ( item ) {
+  if (full() === false){
   basket.push(item);
+} else if (full() === true) {
+  for (i = maxItems-1; i < basket.length; i++ ){
+    let extraItems = basket[i]
+    console.log(`Sorry your cart is full, please remove ${extraItems} from your cart`, listItems(basket));
+    return false;
+  }
+}
   console.log(`The ${item} are added to your basket`)
+  console.log(`There are ${basket.length} items in your basket`);
   return true;
 }
 
@@ -14,6 +24,8 @@ console.log(addItem ('forks'));
 console.log(addItem ('ice'));
 
 console.log(addItem ('oranges'));
+console.log(addItem ('grapes'));
+console.log(addItem ('cake'));
 
 function listItems ( yourBasket ){
   for (const item of yourBasket) {
@@ -21,7 +33,6 @@ function listItems ( yourBasket ){
   }
   return yourBasket;
 }
-
 
 console.log('Your basket contains: ', listItems(basket));
 
@@ -37,8 +48,6 @@ console.log('Your cart is now empty: ', empty());
 console.log(`Adding apples to basket`, addItem('apples'));
 
 console.log(`Basket is now ${basket}`);
-
-const maxItems = 5;
 
 function full() {
   if (basket.length >= maxItems){
