346103f281df1ed7747308440924669674a55d35
SJCMN <sam.clavette@gmail.com>
2021-09-12 15:32:45 -0500

    complete required cart features

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,36 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = []
+
+function addItem ( item ) {
+  basket.push(item);
+  console.log(`The ${item} are added to your basket`)
+  return true;
+}
+
+console.log(addItem ('napkins'));
+console.log(addItem ('forks'));
+console.log(addItem ('ice'));
+
+console.log(addItem ('oranges'));
+
+function listItems ( yourBasket ){
+  for (const item of yourBasket) {
+    console.log(item);
+  }
+  return yourBasket;
+}
+
+console.log('Your basket contains: ', listItems(basket));
+
+function empty (basketArr) {
+  basketArr = [];
+  return basketArr;
+}
+
+console.log('Your cart is now empty: ', empty(basket));
+
+console.log(`Basket is ${basket}`);
+console.log(`Adding apples to basket`, addItem('apples'));
+console.log(`Basket is now ${basket}`);
